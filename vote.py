27716f50c800987c1ff68d7409f8709e24bfa5e2
ripfag <aleksandrbistrov1@bk.ru>
2025-11-22 16:26:02 +0300

    Update vote.py

--- vote.py
+++ vote.py
@@ -1,58 +1,43 @@
-def age():
+def voting():
     while True:
         try:
-            get_age = int(input("Введите ваш возраст:"))
-            break
+            get_age = int(input("Введите ваш возраст: "))
+            if get_age < 18:
+                print("Вы не можете голосовать!")
+                break
+            elif get_age >= 18:
+                print("Подтверждено!")
+
+
+                get_citizen = input("Вы являетесь гражданином страны? да/нет: ").lower().strip()
+                if get_citizen == "да":
+                    print("Потдверждено")
+                elif get_citizen == "нет":
+                    print("Вы не можете голосовать!")
+                    break
+                else:
+                    print("Введите да или нет!")
+                    continue
+
+
+                get_discualified = input("У вас есть запрет голосовать? да/нет: ").lower().strip()
+                if get_discualified == "да":
+                    print("Вы не можете голосовать!")
+                    break
+                elif get_discualified == "нет":
+                    print("Подтверждено")
+                    print("Поздравляем, вы можете голосовать!")
+                    break
+                else:
+                    print("Введите да или нет!")
+                continue
         except ValueError:
             print("Введено не число, введите число!")
-            return
 
 
-    if get_age < 18:
-        print("Вы не можете голосовать!")
-        return False
-    elif get_age >= 18:
-        print("подтверждено")
-        return True
-
-
-def citizen():
-    while True:
-        try:
-            get_citizen = str(input("Вы являетесь гражданином страны?(да/нет)")).strip().lower()
-        except ValueError:
-            print ("Введите да или нет!")
-        except TypeError:
-            print("Введите да или нет!")
-
-
-        if get_citizen == "да":
-            print("подтверждено")
-            return True
-        elif get_citizen == "нет":
-            print("Вы не можете голосовать!")
-            return False
-        else:
-            print("Введите да или нет!")
-
-
-def discualified():
-    while True:
-        try:
-            get_discualified = str(input("У вас есть запрет голосовать? (да/нет)")).strip().lower()
-        except TypeError:
-            print("Введите да или нет!")
-
-
-        if get_discualified == "да":
-            print("Вы не можете голосовать!")
-            return False
-        elif get_discualified == "нет":
-            print("Вы можете голосовать!")
-            return True
-        else:
-            print("Введите да или нет!")
+voting()
 
 
 if age() and citizen() and discualified():
     print("Поздравляем, вы сможете проголосовать!")
+
